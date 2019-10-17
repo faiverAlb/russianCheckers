94bba631447455e545c43a75f88a748b4846b2de
Albert Gizetdinov <faiver.alb@gmail.com>
2019-10-17 21:46:31 +0300

    Add some tests

--- RussianCheckers/RussianCheckersTests/Game/PlayerViewModelTests.cs
+++ RussianCheckers/RussianCheckersTests/Game/PlayerViewModelTests.cs
@@ -475,6 +475,32 @@ namespace RussianCheckers.Game.Tests
             Assert.AreEqual(3 , queueOfDiagonalElements.Count);
         }
 
+        [TestMethod()]
+        public void CalculateAvailableForQueen_Should_Have_2()
+        {
+            //  Arrange
+            var mainPlayCheckers = new List<CheckerElement>()
+            {
+                new CheckerElement(4, 6, PieceType.Queen, Side.White),
+            };
+            var secondPlayerCheckers = new List<CheckerElement>()
+            {
+                new CheckerElement(2, 4, PieceType.Checker, Side.Black),
+            };
+            DataProvider dataProvider = new DataProvider(mainPlayCheckers, secondPlayerCheckers);
+            MainHumanPlayer playerOne = new MainHumanPlayer(Side.White, dataProvider);
+            var playerTwo = new RobotPlayer(Side.Black, dataProvider);
+            var emptyCellsPlayer = new EmptyCellsPlayer(Side.Empty, dataProvider);
+
+            //  Act
+            emptyCellsPlayer.CalculateNeighbors();
+            playerOne.CalculateNeighbors();
+            playerTwo.CalculateNeighbors();
+
+            //  Assert
+            playerOne.CalculateAvailablePaths();
+            Assert.AreEqual(2, playerOne.AvailablePaths.Count);
+        }
 
 
     }

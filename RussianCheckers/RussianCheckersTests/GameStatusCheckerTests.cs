e75ffc0338f458fa6ef6cd2ffa8fad38dc1cdf78
Albert Gizetdinov <faiver.alb@gmail.com>
2019-11-11 08:45:56 +0300

    Add tests

--- RussianCheckers/RussianCheckersTests/GameStatusCheckerTests.cs
+++ RussianCheckers/RussianCheckersTests/GameStatusCheckerTests.cs
@@ -27,6 +27,23 @@ namespace RussianCheckersTests
         }
 
         [TestMethod()]
+        public void GetWinnerSide_NoBlackCheckers_WhiteVictory()
+        {
+            //  Arrange
+            var playerCheckers = new List<CheckerModel> { new CheckerModel(3, 3, PieceType.Checker, Side.White) };
+            DataProvider dataProvider = new DataProvider(playerCheckers, new List<CheckerModel>());
+            var gameStatusChecker = new GameStatusChecker(new MainPlayer(dataProvider, Side.White),
+                new RobotPlayer(dataProvider, Side.Black), new Stack<HistoryMove>());
+            //  Act
+            Side winnerSide = gameStatusChecker.GetWinnerSide();
+
+            //  Assert
+            Assert.AreEqual(Side.White, winnerSide);
+        }
+
+
+
+        [TestMethod()]
         public void GetWinnerSide_PlayersHasPlayableCheckers_NoVictory()
         {
             //  Arrange
@@ -66,20 +83,120 @@ namespace RussianCheckersTests
             Assert.AreEqual(Side.Draw, winnerSide);
         }
 
-        
+
         [TestMethod()]
-        public void GetWinnerSide_NoBlackCheckers_WhiteVictory()
+        public void GetWinnerSide_WhitePlayerHasNoAvailableMoves_BlackPlayerVictory()
         {
             //  Arrange
-            var playerCheckers = new List<CheckerModel>{new CheckerModel(3, 3, PieceType.Checker, Side.White)};
-            DataProvider dataProvider = new DataProvider( playerCheckers, new List<CheckerModel>());
-            var gameStatusChecker = new GameStatusChecker(new MainPlayer(dataProvider, Side.White),
-                new RobotPlayer(dataProvider, Side.Black), new Stack<HistoryMove>());
+            var playerCheckersFirstPlayer = new List<CheckerModel>{new CheckerModel(0, 6, PieceType.Checker, Side.White)};
+            var playerCheckersSecondPlayer = new List<CheckerModel>{new CheckerModel(1, 7, PieceType.Checker, Side.Black)};
+            DataProvider dataProvider = new DataProvider( playerCheckersFirstPlayer, playerCheckersSecondPlayer);
+            var mainPlayer = new MainPlayer(dataProvider, Side.White);
+            var robotPlayer = new RobotPlayer(dataProvider, Side.Black);
+
             //  Act
+            mainPlayer.CalculateNeighbors();
+            robotPlayer.CalculateNeighbors();
+            mainPlayer.CalculateAvailablePaths();
+            robotPlayer.CalculateAvailablePaths();
+            var gameStatusChecker = new GameStatusChecker(mainPlayer, robotPlayer, new Stack<HistoryMove>());
             Side winnerSide = gameStatusChecker.GetWinnerSide();
-            
+
+            //  Assert
+            Assert.AreEqual(Side.Black, winnerSide);
+        }
+
+        [TestMethod()]
+        public void GetWinnerSide_BlackPlayerHasNoAvailableMoves_WhitePlayerVictory()
+        {
+            //  Arrange
+            var playerCheckersFirstPlayer = new List<CheckerModel>{new CheckerModel(0, 2, PieceType.Checker, Side.Black) };
+            var playerCheckersSecondPlayer = new List<CheckerModel>{new CheckerModel(1, 1, PieceType.Checker, Side.White), new CheckerModel(2, 0, PieceType.Checker, Side.White) };
+            DataProvider dataProvider = new DataProvider( playerCheckersFirstPlayer, playerCheckersSecondPlayer);
+            var mainPlayer = new MainPlayer(dataProvider, Side.White);
+            var robotPlayer = new RobotPlayer(dataProvider, Side.Black);
+
+            //  Act
+            mainPlayer.CalculateNeighbors();
+            robotPlayer.CalculateNeighbors();
+            mainPlayer.CalculateAvailablePaths();
+            robotPlayer.CalculateAvailablePaths();
+            var gameStatusChecker = new GameStatusChecker(mainPlayer, robotPlayer, new Stack<HistoryMove>());
+            Side winnerSide = gameStatusChecker.GetWinnerSide();
+
             //  Assert
             Assert.AreEqual(Side.White, winnerSide);
+        }
+
+
+        [TestMethod()]
+        public void GetWinnerSide_QueensMoved15TimesWithoutTaking_Draw()
+        {
+            //  Arrange
+            var playerCheckersFirstPlayer = new List<CheckerModel> {new CheckerModel(0, 0, PieceType.Queen, Side.White) };
+            var playerCheckersSecondPlayer = new List<CheckerModel> {new CheckerModel(7, 1, PieceType.Queen, Side.Black) };
+            DataProvider dataProvider = new DataProvider( playerCheckersFirstPlayer, playerCheckersSecondPlayer);
+            var mainPlayer = new MainPlayer(dataProvider, Side.White);
+            var robotPlayer = new RobotPlayer(dataProvider, Side.Black);
+            var actionsHistory = new Stack<HistoryMove>(GetHistoryActionsWithoutTaking(playerCheckersFirstPlayer.Single(), playerCheckersSecondPlayer.Single()));
+
+            //  Act
+            mainPlayer.CalculateNeighbors();
+            robotPlayer.CalculateNeighbors();
+            mainPlayer.CalculateAvailablePaths();
+            robotPlayer.CalculateAvailablePaths();
+            var gameStatusChecker = new GameStatusChecker(mainPlayer, robotPlayer, actionsHistory);
+            Side winnerSide = gameStatusChecker.GetWinnerSide();
+
+            //  Assert
+            Assert.AreEqual(Side.Draw, winnerSide);
+        }
+
+
+        [TestMethod()]
+        public void GetWinnerSide_QueensMoved15TimesWithTaking_NoDraw()
+        {
+            //  Arrange
+            var playerCheckersFirstPlayer = new List<CheckerModel> { new CheckerModel(0, 0, PieceType.Queen, Side.White) };
+            var playerCheckersSecondPlayer = new List<CheckerModel> { new CheckerModel(7, 1, PieceType.Queen, Side.Black) };
+            DataProvider dataProvider = new DataProvider(playerCheckersFirstPlayer, playerCheckersSecondPlayer);
+            var mainPlayer = new MainPlayer(dataProvider, Side.White);
+            var robotPlayer = new RobotPlayer(dataProvider, Side.Black);
+            var actionsHistory = new Stack<HistoryMove>(GetHistoryActionsWithTaking(playerCheckersFirstPlayer.Single(), playerCheckersSecondPlayer.Single()));
+
+            //  Act
+            mainPlayer.CalculateNeighbors();
+            robotPlayer.CalculateNeighbors();
+            mainPlayer.CalculateAvailablePaths();
+            robotPlayer.CalculateAvailablePaths();
+            var gameStatusChecker = new GameStatusChecker(mainPlayer, robotPlayer, actionsHistory);
+            Side winnerSide = gameStatusChecker.GetWinnerSide();
+
+            //  Assert
+            Assert.AreEqual(Side.None, winnerSide);
+        }
+
+
+        private IEnumerable<HistoryMove> GetHistoryActionsWithoutTaking(CheckerModel firstQueen, CheckerModel secondQueen)
+        {
+            var actionsHistory = new List<HistoryMove>();
+            for (int i = 0; i < 8; i++)
+            {
+                actionsHistory.Add(new HistoryMove(false) { MovedFromTo = new KeyValuePair<CheckerModel, CheckerModel>(firstQueen, new CheckerModel(1, 1, PieceType.Checker, Side.Empty)) });
+                actionsHistory.Add(new HistoryMove(false) { MovedFromTo = new KeyValuePair<CheckerModel, CheckerModel>(secondQueen, new CheckerModel(2, 6, PieceType.Checker, Side.Empty)) });
+            }
+            return actionsHistory;
+        }
+
+        private IEnumerable<HistoryMove> GetHistoryActionsWithTaking(CheckerModel firstQueen, CheckerModel secondQueen)
+        {
+            var actionsHistory = new List<HistoryMove>();
+            for (int i = 0; i < 8; i++)
+            {
+                actionsHistory.Add(new HistoryMove(false) { MovedFromTo = new KeyValuePair<CheckerModel, CheckerModel>(firstQueen, new CheckerModel(1, 1, PieceType.Checker, Side.Empty)),DeletedList = { new KeyValuePair<CheckerModel, CheckerModel>(new CheckerModel(2,2,PieceType.Checker,Side.Black), new CheckerModel(2, 2, PieceType.Checker, Side.Black)) }});
+                actionsHistory.Add(new HistoryMove(false) { MovedFromTo = new KeyValuePair<CheckerModel, CheckerModel>(secondQueen, new CheckerModel(2, 6, PieceType.Checker, Side.Empty)), DeletedList = { new KeyValuePair<CheckerModel, CheckerModel>(new CheckerModel(2, 2, PieceType.Checker, Side.White), new CheckerModel(2, 2, PieceType.Checker, Side.White)) } });
+            }
+            return actionsHistory;
         }
 
     }
